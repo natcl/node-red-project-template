a44f161ae4de8335f81136eb1f8cfc2581e241e3
Nathanaël Lécaudé <natcl@users.noreply.github.com>
2016-05-02 10:56:37 -0400

    Update start.js

--- start.js
+++ start.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
 const spawn = require('child_process').spawn;
+const exec = require('child_process').execSync;
 
 var port = parseInt(process.argv[2]);
 var command;
+
+// Optional, perform an npm install first
+//const npm_install = exec('npm install -f');
 
 if (port) {
   command = spawn('node', ['node_modules/node-red/red.js', '-v', '-p', port, '-userDir', '.']);
